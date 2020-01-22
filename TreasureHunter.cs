45df9db2c5d4e9e9ea82f7ee9963622eb840be1f
awskmet <54879433+awskmet@users.noreply.github.com>
2020-01-21 20:13:12 -0500

    Create TreasureHunter.cs

---
+++ TreasureHunter.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Rendering.PostProcessing;
+
+public class TreasureHunter : MonoBehaviour
+{
+    OVRCameraRig oVRCameraRig;
+
+    OVRManager oVRManager;
+
+    OVRHeadsetEmulator oVRHeadsetEmulator;
+
+    Camera viewPointCamera;
+
+    /*PostProcessLayer postProcessLayer;
+    LocomotionHandler locomotionHandler;
+    public Inventory inv;
+    public Collectable Collectable1;
+    Start is called before the first frame update*/
+
+    int currentTotalScore;
+    int currentTotalTreasures;
+    void Start()
+    {
+       //inv = new Inventory();
+       //Collectable1 = new Collectable();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            this.gameObject.GetComponent<Inventory>().setInv(0, (GameObject.Find("Hunted1").GetComponent<Collectable>()));
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2)){
+            this.gameObject.GetComponent<Inventory>().setInv(1, (GameObject.Find("Hunted2").GetComponent<Collectable>()));
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3)){
+            this.gameObject.GetComponent<Inventory>().setInv(2, (GameObject.Find("Hunted3").GetComponent<Collectable>()));
+        }
+
+         if (Input.GetKeyDown(KeyCode.Alpha4)){
+            CalculateScore();
+            int tScore=currentTotalScore;
+            int tTreas=currentTotalTreasures;
+            GameObject.Find("ScoreMessage").GetComponent<TextMesh>().text=("Anthony Doban\n"+"score = "+tScore+"\ncount = "+tTreas+" treasures");
+    }
+         if (CalculateScore()==111){
+            GameObject.Find("WinMessage").GetComponent<TextMesh>().text=("you win");  //make it so u can keep playing
+            }
+        
+    }
+
+    // Update is called once per frame
+    int CalculateScore(){
+        Collectable[] collTreasure = this.gameObject.GetComponent<Inventory>().hunterInventory;
+        int totalScore=0;
+        int treasureAmt=0;
+        foreach (Collectable treasure in collTreasure){
+            if (treasure!=null){
+                treasureAmt+=1;
+                totalScore+=treasure.points;
+            }
+        }
+        currentTotalScore=totalScore;
+        currentTotalTreasures=treasureAmt;
+        return totalScore;
+        
+    
+        
+    }
+    
+}
